dc185717ecb67535eb2788bf2f40374520eaa679
Ben Calow <ben@calow.com>
2025-10-04 16:31:34 -0600

    Restoring

--- core/solver.py
+++ core/solver.py
@@ -2,7 +2,7 @@ import physics
 
 
 eqVars_dict = {
-    "specificGasContent"    :   frozenset({"R", "M"}),                                              #Specific Gas Constant
+    "specificGasContent"    :   frozenset(frozenset(), frozenset({"R", "M"})),                                              #Specific Gas Constant
     "temperatureRatio"      :   frozenset({"Ma", "T", "T_s", "gamma"}),                             #Isentropic Temperature Ratio
     "pressureRatio"         :   frozenset({"Ma", "P", "P_s", "gamma"}),                             #Isentropic Pressure Ratio
     "areaMachRelation"      :   frozenset({"A_x", "A_y", "Ma_x", "Ma_y", "gamma"}),                 #Area-Mach Relation
@@ -18,12 +18,11 @@ def reciever(inputVars, derivedVars):
     while running:
         running = False
         for equation_id, equationVars in eqVars_dict.items():
-            for subequationVars in equationVars:
-                unknownVars = subequationVars - list(inputVars.keys) - list(derivedVars.keys)
-                knownVars = subequationVars - unknownVars
-                if len(unknownVars) == 1:
-                    physicsSolver(equation_id, knownVars)
-                    running = True
+            unknownVars = equationVars - list(inputVars.keys) - list(derivedVars.keys)
+            knownVars = equationVars - unknownVars
+            if len(unknownVars) == 1:
+                physicsSolver(equation_id, knownVars)
+                running = True
 
 
 def physicsSolver(equation_id, knownVars):
