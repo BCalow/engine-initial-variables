e318d796eb678bc53e2f2ec5ecafcf29e8afce6b
Ben Calow <ben@calow.com>
2025-10-06 16:05:13 -0600

    Begining GUI

---
+++ core/__init__.py
@@ -0,0 +1,13 @@
+from .physics import (
+    specificGasConstant,
+    temperatureRatio,
+    pressureRatio,
+    areaMachRelation,
+    exitVelocity,
+    massFlow,
+    thrust,
+)
+from .solver import (
+    equationSolver
+)
+from .units import *
--- ui/main.py
+++ ui/main.py
@@ -1,1 +1,22 @@
-#Main GUI
+from PyQt6.QtWidgets import (
+							QApplication, 
+							QWidget, 
+							QVBoxLayout,
+							QCheckBox, 
+							QLabel,
+							QGridLayout,
+							QHBoxLayout,
+							QLineEdit,
+							QGroupBox,
+							QSizePolicy,
+							QSpacerItem,
+)
+from PyQt6.QtGui import(
+							QFontMetrics
+)
+from PyQt6.QtCore import(
+							Qt,
+							pyqtSignal
+)
+import sys
+
